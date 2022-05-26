af684deb2b37b9e679ac9e3d46bde18cdf4cf04b
Eric Burch <jameseburcham@gmail.com>
2022-05-26 13:17:00 -0500

    add tooling and a test project

---
+++ build.cake
@@ -0,0 +1,1 @@
+#addin "nuget:?package=Newtonsoft.Json&version=13.0.1"
---
+++ docfx_project/src/TestCakeDocFx/TestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace TestCakeDocFx
+{
+    /// <summary>
+    ///     This is the test class.
+    /// </summary>
+    public class TestClass
+    {
+        /// <summary>
+        ///     This is the test value.
+        /// </summary>
+        public string TestValue { get; set; }
+    }
+}
